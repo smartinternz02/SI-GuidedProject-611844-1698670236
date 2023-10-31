cb103de89278e242ed98b6a9d924347290a2ec47
Sparsh Maheshwari <sparshmaheshwari15@gmail.com>
2023-10-31 17:57:51 +0530

    added python file

--- a.py
+++ a.py
@@ -1,1 +1,15 @@
-print("try")
+import requests
+from bs4 import BeautifulSoup
+
+while(True):
+    find=input()
+    url=f"https://www.google.com/search?q={find}"
+    # url=f"https://en.wikipedia.org/wiki/{find}"
+
+    req=requests.get(url)
+    soup=BeautifulSoup(req.text,"html.parser")
+
+    # print(req.text)
+    mysearch=soup.find("div",class_="BNeawe").text
+    # mysearch=soup.find("div",class_="mw-parser-output").text
+    print(mysearch)
